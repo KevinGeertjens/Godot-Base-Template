4d5ac735f9b12985c3785181983e29abea14ce19
KevinGeertjens <K.Geertjens@gmail.com>
2024-06-26 19:19:51 +0200

    Fix missing controller controls
    Several controls weren't enabled by default for controllers. Also created custom action for ingame menu, as ui_cancel uses the B button (xbox) which could be used for gameplay instead

--- code/scenes/gameplay/gui/gui.gd
+++ code/scenes/gameplay/gui/gui.gd
@@ -3,6 +3,6 @@ extends CanvasLayer
 @onready var pauseMenu = $PauseMenu
 
 func _process(_delta):
-	if !pauseMenu.visible && Input.is_action_just_released("ui_cancel"):
+	if !pauseMenu.visible && Input.is_action_just_released("ui_game_menu"):
 		get_tree().paused = true
 		pauseMenu.visible = true
